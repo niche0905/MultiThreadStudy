d0447535edfad20c610c3c707e327004e1c545d8
niche0905 <yudle537@gmail.com>
2024-12-20 21:14:26 +0900

    LF SkipList 초안
    메모리 누수가 있고 EBR을 적용하기 전 코드이다

---
+++ MultiThreadStudy/SkipListEBR.cpp
@@ -0,0 +1,451 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <array>
+#include <numeric>
+#include <atomic>
+
+
+constexpr int MAX_TOP{ 9 };
+
+class SK_LF_NODE;		// SkipList LockFree Node 전방 선언
+
+class SK_SPTR			// SkipList 합성 포인터 (LockFree(CAS)를 위한)
+{
+private:
+	std::atomic<long long> sptr;
+
+public:
+	SK_SPTR() : sptr{ 0 } {}
+
+	void set_ptr(SK_LF_NODE* ptr)
+	{
+		sptr = reinterpret_cast<long long>(ptr);
+	}
+
+	SK_LF_NODE* get_ptr()
+	{
+		long long p = sptr.load();
+		return reinterpret_cast<SK_LF_NODE*>(p & 0xFFFFFFFFFFFFFFFE);
+	}
+
+	SK_LF_NODE* get_ptr(bool* removed)
+	{
+		long long p = sptr.load();
+		*removed = (1 == (p & 1));	// p의 최하위 1비트로 removed를 판단
+		return reinterpret_cast<SK_LF_NODE*>(p & 0xFFFFFFFFFFFFFFFE);
+	}
+
+	bool CAS(SK_LF_NODE* old_p, SK_LF_NODE* new_p, bool old_m, bool new_m)
+	{
+		long long old_v = reinterpret_cast<long long>(old_p);
+		if (true == old_m) old_v = old_v | 1;
+		else old_v = old_v & 0xFFFFFFFFFFFFFFFE;	// 굳이 필요한 작업인지는 잘 모르겠습니다
+
+		long long new_v = reinterpret_cast<long long>(new_p);
+		if (true == new_m) new_v = new_v | 1;
+		else new_v = new_v & 0xFFFFFFFFFFFFFFFE;
+
+		return std::atomic_compare_exchange_strong(&sptr, &old_v, new_v);
+		// &sptr 위치에 있는 값을 old_v와 비교 후 같다면 new_v로 달라서 바꾸지 못했으면 false를 반환 후 &sptr에 있던 값은 old_v로 확인 가능
+	}
+};
+
+class SK_LF_NODE		// SkipList LockFree Node 
+{
+public:
+	int key;
+	SK_SPTR* volatile next[MAX_TOP + 1] = {};	// 0층부터 9층(MAX_TOP)까지 있음
+	int top_level;	// 현재 노드의 최상층 (지름길 존재하는 층)
+
+public:
+	SK_LF_NODE(int x, int top) : key{ x }, top_level{ top }
+	{
+		for (int i = 0; i <= top_level; ++i) {
+			next[i] = new SK_SPTR();
+		}
+	}
+	
+	~SK_LF_NODE()
+	{
+		for (int i = 0; i <= top_level; ++i) {
+			delete next[i];
+		}
+	}
+};
+
+class LF_SK_SET			// SkipList LockFree Set
+{
+private:
+	SK_LF_NODE head{ std::numeric_limits<int>::min(), MAX_TOP };
+	SK_LF_NODE tail{ std::numeric_limits<int>::max(), MAX_TOP };
+
+public:
+	LF_SK_SET()
+	{
+		Init();
+	}
+
+	void Init()
+	{
+		for (int i = 0; i <= MAX_TOP; ++i) {
+			head.next[i]->set_ptr(&tail);
+		}
+	}
+
+	void Clear()
+	{
+		// 0층으로 순회하면서 삭제
+		while (head.next[0]->get_ptr() != &tail) {
+			auto p = head.next[0]->get_ptr();
+			head.next[0]->set_ptr(head.next[0]->get_ptr()->next[0]->get_ptr());
+			delete p;
+		}
+
+		// 모두 삭제 후 head의 next는 tail로 설정
+		Init();
+	}
+
+	bool Find(int x, SK_LF_NODE* prevs[], SK_LF_NODE* currs[])
+	{
+	retry:
+
+		// 최상층 (지름길 부터 탐색)
+		for (int i = MAX_TOP; i >= 0; --i) {
+			if (i == MAX_TOP) {		// 최상층 인 경우 받아올 상위 층이 없어서 조건문
+				prevs[i] = &head;
+			}
+			else {		// 최상층이 아니면 이전 상위 층으로 부터 Node 가져옴
+				prevs[i] = prevs[i + 1];
+			}
+
+			currs[i] = prevs[i]->next[i]->get_ptr();
+
+			while (true) {
+				bool removed = false;
+				SK_LF_NODE* succ = currs[i]->next[i]->get_ptr(&removed);
+				
+				// 순회 중 삭제되었다면 물리적으로 삭제 시도
+				while (true == removed) {
+					// 내가 삭제하지 못했다면 retry
+					if (false == prevs[i]->next[i]->CAS(currs[i], succ, false, false)) {
+						goto retry;
+					}
+
+					// 내가 삭제했다면 이어서 탐색
+					currs[i] = succ;
+					succ = currs[i]->next[i]->get_ptr(&removed);
+				}
+
+				// 탐색한 curr가 찾으려는 값 이상이라면 다음층 또는 종결
+				if (currs[i]->key >= x)
+					break;
+
+				// 아니라면 이어서 순회
+				prevs[i] = currs[i];
+				currs[i] = succ;
+			}
+		}
+
+		return (currs[0]->key == x);
+	}
+
+	bool Add(int x)
+	{
+		while (true) {
+			SK_LF_NODE* prevs[MAX_TOP + 1];
+			SK_LF_NODE* currs[MAX_TOP + 1];
+
+			bool found = Find(x, prevs, currs);
+
+			// 이미 존재한다면
+			if (true == found)
+				return false;
+
+			// 생성할 노드의 레벨 (코인 토스)
+			int lv = 0;
+			for (int i = 0; i < MAX_TOP; ++i) {	// 이거 조건문이 <= 여야 MAX_TOP 까지 하는 것이 아닌가?
+				if (rand() % 2 == 0) break;
+				++lv;
+			}
+
+			SK_LF_NODE* new_node = new SK_LF_NODE{ x, lv };
+
+			for (int i = 0; i <= lv; ++i) {
+				new_node->next[i]->set_ptr(currs[i]);
+			}
+
+			// 최하층을 내가 추가했으면(CAS에 성공했으면 내가 추가한 것임)
+			if (false == prevs[0]->next[0]->CAS(currs[0], new_node, false, false)) {	// 어떠한 이유로 CAS에 실패하였으므로 Find 부터 다시
+				delete new_node;
+				continue;
+			}
+
+			// 내가 추가했으면 위에 층들도 책임지고 연결
+			for (int i = 1; i <= lv; ++i) {
+				while (true) {
+					if (true == prevs[i]->next[i]->CAS(currs[i], new_node, false, false))
+						break;
+
+					Find(x, prevs, currs);
+					//new_node->next[i]->set_ptr(currs[i]);	// 교재에는 버그로 없다 -> 없으면 EBR에서 오류 // 하지만 지금은 있으면 안돌아가기에 주석 처리
+				}
+			}
+
+			return true;
+		}
+	}
+
+	bool Remove(int x)
+	{
+		while (true) {
+			SK_LF_NODE* prevs[MAX_TOP + 1];
+			SK_LF_NODE* currs[MAX_TOP + 1];
+
+			bool found = Find(x, prevs, currs);
+
+			// 존재하지 않는다면
+			if (false == found)
+				return false;
+
+			// 찾았으므로 삭제 과정 시작
+			SK_LF_NODE* del_node = currs[0];
+			int lv = del_node->top_level;
+
+			// 최상층 부터 remove 진행
+			for (int i = lv; i > 0; --i) {
+				bool removed = false;
+				SK_LF_NODE* succ = del_node->next[i]->get_ptr(&removed);
+
+				// 삭제 되었는지 확인하면서 삭제 시도
+				while (false == removed) {	// 삭제가 안되었다면 삭제 시도 (CAS)
+					del_node->next[i]->CAS(succ, succ, false, true);	// CAS의 성공을 확인하지 않아도 누군가 Removed 했으면 다음 아래 층으로 이동하기 위해
+					succ = del_node->next[i]->get_ptr(&removed);
+				}
+			}
+
+			SK_LF_NODE* succ = del_node->next[0]->get_ptr();
+			while (true) {
+				bool marking = del_node->next[0]->CAS(succ, succ, false, true);
+
+				// 내가 최하층 마킹(삭제)를 성공하면 내가 삭제한 것!
+				if (marking) {
+					//delete del_node;	// 메모리 릭 <- Find에서 물리적인 제거 후 해야하지만 거기서도 하지 못함
+					Find(x, prevs, currs);	// 연결리스트 정리 작업용
+					return true;
+				}
+
+				bool removed = false;
+				succ = del_node->next[0]->get_ptr(&removed);
+				// 제거가 되었는데 나는 CAS에 실패했다 (다른놈이 마킹에 성공한 것)
+				if (true == removed)
+					return false;
+
+				// 누군가 Add 등 연결리스트 구조가 바뀌어서 CAS에 실패한 것... 다시 시도
+			}
+		}
+	}
+
+	bool Contains(int x)
+	{
+		// Find와 다르게 CAS를 시도하고 goto로 다시 시도하지 않는다, WaitFree..?
+		SK_LF_NODE* prevs[MAX_TOP + 1];
+		SK_LF_NODE* currs[MAX_TOP + 1];
+
+		for (int i = MAX_TOP; i >= 0; --i) {
+			if (i == MAX_TOP) {
+				prevs[i] = &head;
+			}
+			else {
+				prevs[i] = prevs[i + 1];
+			}
+
+			currs[i] = prevs[i]->next[i]->get_ptr();
+
+			while (true)
+			{
+				bool removed = false;
+				SK_LF_NODE* succ = currs[i]->next[i]->get_ptr(&removed);
+				while (true == removed) {
+					currs[i] = succ;
+					succ = currs[i]->next[i]->get_ptr(&removed);
+				}
+
+				if (currs[i]->key >= x)
+					break;
+
+				prevs[i] = currs[i];
+				currs[i] = succ;
+			}
+		}
+
+		return (currs[0]->key == x);
+	}
+
+	void Print20()
+	{
+		auto p = head.next[0]->get_ptr();
+		for (int i = 0; i < 20; ++i) {
+			if (p == &tail) break;
+			std::cout << p->key << ", ";
+			p = p->next[0]->get_ptr();
+		}
+
+		std::cout << std::endl;
+	}
+};
+
+#define MY_SET LF_SK_SET
+MY_SET my_set;
+
+constexpr int MAX_THREAD{ 16 };
+constexpr int NUM_TEST{ 4000000 };
+constexpr int KEY_RANGE{ 1000 };
+
+
+// 교수님 오류체크 코드
+class HISTORY {
+public:
+	int op;
+	int i_value;
+	bool o_value;
+	HISTORY(int o, int i, bool re) : op(o), i_value(i), o_value(re) {}
+};
+
+std::array<std::vector<HISTORY>, 16> history;
+
+void worker_check(int num_threads, int thread_id)
+{
+	for (int i = 0; i < NUM_TEST / num_threads; ++i) {
+		int op = rand() % 3;
+		switch (op) {
+		case 0: {
+			int v = rand() % KEY_RANGE;
+			history[thread_id].emplace_back(0, v, my_set.Add(v));
+			break;
+		}
+		case 1: {
+			int v = rand() % KEY_RANGE;
+			history[thread_id].emplace_back(1, v, my_set.Remove(v));
+			break;
+		}
+		case 2: {
+			int v = rand() % KEY_RANGE;
+			history[thread_id].emplace_back(2, v, my_set.Contains(v));
+			break;
+		}
+		}
+	}
+}
+
+void check_history(int num_threads)
+{
+	std::array <int, KEY_RANGE> survive = {};
+	std::cout << "Checking Consistency : ";
+	if (history[0].size() == 0) {
+		std::cout << "No history.\n";
+		return;
+	}
+	for (int i = 0; i < num_threads; ++i) {
+		for (auto& op : history[i]) {
+			if (false == op.o_value) continue;
+			if (op.op == 3) continue;
+			if (op.op == 0) survive[op.i_value]++;
+			if (op.op == 1) survive[op.i_value]--;
+		}
+	}
+	for (int i = 0; i < KEY_RANGE; ++i) {
+		int val = survive[i];
+		if (val < 0) {
+			std::cout << "ERROR. The value " << i << " removed while it is not in the set.\n";
+			exit(-1);
+		}
+		else if (val > 1) {
+			std::cout << "ERROR. The value " << i << " is added while the set already have it.\n";
+			exit(-1);
+		}
+		else if (val == 0) {
+			if (my_set.Contains(i)) {
+				std::cout << "ERROR. The value " << i << " should not exists.\n";
+				exit(-1);
+			}
+		}
+		else if (val == 1) {
+			if (false == my_set.Contains(i)) {
+				std::cout << "ERROR. The value " << i << " shoud exists.\n";
+				exit(-1);
+			}
+		}
+	}
+	std::cout << " OK\n";
+}
+
+// 벤치마크 코드
+void benchmark(const int num_thread)
+{
+	int key;
+
+	for (int i = 0; i < NUM_TEST / num_thread; i++) {
+		switch (rand() % 3) {
+		case 0: key = rand() % KEY_RANGE;
+			my_set.Add(key);
+			break;
+		case 1: key = rand() % KEY_RANGE;
+			my_set.Remove(key);
+			break;
+		case 2: key = rand() % KEY_RANGE;
+			my_set.Contains(key);
+			break;
+		default: std::cout << "Error\n";
+			exit(-1);
+		}
+	}
+}
+
+
+int main()
+{
+	using namespace std::chrono;
+
+	// 오류 검사 부분
+	{
+		for (int n = 1; n <= MAX_THREAD; n = n * 2) {
+			for (auto& v : history)
+				v.clear();
+			std::vector<std::thread> tv;
+			auto start_t = high_resolution_clock::now();
+			for (int i = 0; i < n; ++i) {
+				tv.emplace_back(worker_check, n, i);
+			}
+			for (auto& th : tv)
+				th.join();
+			auto end_t = high_resolution_clock::now();
+			auto exec_t = end_t - start_t;
+			size_t ms = duration_cast<milliseconds>(exec_t).count();
+			std::cout << n << " Threads,  " << ms << "ms.";
+			check_history(n);
+			my_set.Clear();
+		}
+	}
+
+	// 실제 성능 측정
+	{
+		for (int n = 1; n <= MAX_THREAD; n = n * 2) {
+			std::vector<std::thread> tv;
+			auto start_t = high_resolution_clock::now();
+			for (int i = 0; i < n; ++i) {
+				tv.emplace_back(benchmark, n);
+			}
+			for (auto& th : tv)
+				th.join();
+			auto end_t = high_resolution_clock::now();
+			auto exec_t = end_t - start_t;
+			size_t ms = duration_cast<milliseconds>(exec_t).count();
+			std::cout << n << " Threads,  " << ms << "ms.";
+			my_set.Print20();
+			my_set.Clear();
+		}
+
+	}
+}
