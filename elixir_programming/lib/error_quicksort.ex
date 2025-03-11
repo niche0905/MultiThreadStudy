2d5c698a5eaad0448c32451d1b7f4bad52216481
niche0905 <yudle537@gmail.com>
2025-03-11 18:04:09 +0900

    공백이 문제였다
    놀랍게도 함수에 공백이 문제여서 제대로 돌아가지 않았다

--- elixir_programming/lib/error_quicksort.ex
+++ elixir_programming/lib/error_quicksort.ex
@@ -1,8 +1,8 @@
 defmodule ErrorQuicksort do
   def sort([]), do: []
-  def sort ([pivot|t]) do
-    sort (for x <- t, x < pivot, do: x)
+  def sort([pivot|t]) do
+    sort(for x <- t, x < pivot, do: x)
     ++ [pivot] ++
-    sort (for x <- t, x >= pivot, do: x)  # 문제가 되는 코드
+    sort(for x <- t, x >= pivot, do: x)  # 문제가 되는 코드
   end
 end
