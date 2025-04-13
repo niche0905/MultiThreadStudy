6045f666b754d2c00b6876523b90accd5acb7f55
niche0905 <yudle537@gmail.com>
2025-04-13 22:18:23 +0900

    C++ Factorize 반복
    C++ Factorize 코드를 반복 테스트가 가능하게 약간 수정하였다

--- MultiThreadStudy/Factorize.cpp
+++ MultiThreadStudy/Factorize.cpp
@@ -41,41 +41,44 @@ void worker(int th_id, int p)
 int main()
 {
 	using namespace std::chrono;
-	std::cout << "Enter a number : ";
-	std::cin >> g_n;
 
-	auto start_t = system_clock::now();
-	long long sqrt_n = sqrt(g_n);
+	while (true) {
+		std::cout << "Enter a number : ";
+		std::cin >> g_n;
 
-	std::vector <std::thread> workers;
-	int index = 0;
-	worker(0, 2);
-	for (int p = 3; p < sqrt_n; p = p + 2) {
-		if (erased_list.count(p) != 0)
-			continue;
-		workers.emplace_back(worker, index, p);
-		index++;
+		auto start_t = system_clock::now();
+		long long sqrt_n = sqrt(g_n);
 
-		while (index - done_threads.size() >= std::thread::hardware_concurrency()) {
-			//std::this_thread::sleep_for(1ms);
-			std::this_thread::yield();
-			sqrt_n = sqrt(g_n);
+		std::vector <std::thread> workers;
+		int index = 0;
+		worker(0, 2);
+		for (int p = 3; p < sqrt_n; p = p + 2) {
+			if (erased_list.count(p) != 0)
+				continue;
+			workers.emplace_back(worker, index, p);
+			index++;
+
+			while (index - done_threads.size() >= std::thread::hardware_concurrency()) {
+				//std::this_thread::sleep_for(1ms);
+				std::this_thread::yield();
+				sqrt_n = sqrt(g_n);
+			}
+
 		}
 
+		for (auto& th : workers)
+			th.join();
+		auto end_t = system_clock::now();
+		auto exec_t = end_t - start_t;
+		auto ms = duration_cast<milliseconds>(exec_t).count();
+
+		std::cout << "Exec Time : " << ms << "ms.  Result = ";
+		for (auto n : factors)
+			std::cout << n << ", ";
+
+		std::cout << g_n << std::endl << std::endl;
+		erased_list.clear(); done_threads.clear(); factors.clear();
 	}
-
-	for (auto& th : workers)
-		th.join();
-	auto end_t = system_clock::now();
-	auto exec_t = end_t - start_t;
-	auto ms = duration_cast<milliseconds>(exec_t).count();
-
-	std::cout << "Exec Time : " << ms << "ms.  Result = ";
-	for (auto n : factors)
-		std::cout << n << ", ";
-
-	std::cout << g_n << std::endl;
-
 
 }
 
