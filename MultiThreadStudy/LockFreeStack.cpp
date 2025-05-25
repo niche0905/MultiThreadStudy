04f3501893908127b5a51d0ab43423d71741acf2
niche0905 <yudle537@gmail.com>
2025-05-25 22:30:59 +0900

    코드 위치 수정

--- MultiThreadStudy/LockFreeStack.cpp
+++ MultiThreadStudy/LockFreeStack.cpp
@@ -252,6 +252,181 @@ struct RangePolicy
 thread_local int thread_id;	// 스레드 ID
 thread_local RangePolicy range(2 * now_thread_num);
 std::atomic_int g_el_success = 0;
+struct LockFreeEliminationStack
+{
+	Node* volatile top;
+	std::vector<ThreadInfoPtr> location;
+	std::vector<Integer> collision;
+
+	LockFreeEliminationStack(int th_num = MAX_THREADS) : top(nullptr), location(th_num), collision(2 * th_num, -1) {}
+
+	bool CAS(Node* old_val, Node* new_val)
+	{
+		return std::atomic_compare_exchange_strong(
+			reinterpret_cast<volatile std::atomic_llong*>(&top),
+			reinterpret_cast<long long*>(&old_val),
+			reinterpret_cast<long long>(new_val));
+	}
+
+	int GetPosition()
+	{
+		return (rand() % range.current_range);	// 중앙 집중 방식 (결론적으로)
+		/*	복잡만 하고 위와 비슷한 일을 하는 코드이다
+		int mid = range.max_range / 2;
+		int half_range = range.current_range / 2;
+		if (half_range == 0) return mid;							// 범위가 너무 작음 (zero division 방지)
+		return (rand() % range.current_range) + (mid - half_range);	// 중앙 집중 분포로 서브 논문의 방식
+																	// woudenberg 논문의 11page에서 중앙 집중 분포를 사용한 성능 비교 참고
+																	// 메인 논문에도 있었다 (배열의 중앙 집중 방식이지만 Range를 늘려감)
+		*/
+	}
+
+	void Clear()
+	{
+		while (top != nullptr) {
+			Node* temp = top;
+			top = top->next;
+			delete temp;
+		}
+	}
+
+	void Push(int num)
+	{
+		Node* new_node = new Node(num);
+		ThreadInfo* p = new ThreadInfo{ thread_id, 'P', new_node };	// 스레드 정보 저장
+
+		while (true) {
+			Node* old_top = top;
+			new_node->next = old_top;
+			if (true == CAS(old_top, new_node)) {
+				return;	// 성공적으로 푸시됨 (중앙 스택에 바로,,, 가장 깔끔한 적은 부하의 상황)
+			}
+			// 실패한 경우
+
+			// 충돌 시도
+			ThreadInfo* matched_info = nullptr;
+			location[thread_id].ptr = p;
+			if (TryEliminate(p, matched_info)) {
+				range.expand();
+				g_el_success++;
+				return;	// 소거 성공
+			}
+			else {
+				range.shrink();	// 범위 축소
+			}
+		}
+	}
+
+	int Pop()
+	{
+		ThreadInfo* p = new ThreadInfo{ thread_id, 'Q', nullptr };	// 스레드 정보 저장
+
+		while (true) {
+			Node* old_top = top;
+			if (old_top == nullptr) {
+				return EMPTY;	// 비어있음
+			}
+			Node* new_top = old_top->next;
+			if (CAS(old_top, new_top)) {
+				int num = old_top->key;
+				//delete old_top;	// 노드 삭제
+				return num;	// 성공적으로 pop 됨
+			}
+
+			ThreadInfo* matched_info = nullptr;
+			location[thread_id].ptr = p;
+			if (TryEliminate(p, matched_info)) {
+				int num = matched_info->node->key;	// p노드가 nullptr임 지역 변수가 사라진 듯 함
+				range.expand();
+				//delete p.node;
+				return num;
+			}
+			else {
+				range.shrink();	// 범위 축소
+			}
+		}
+	}
+
+	void Print20()
+	{
+		std::cout << "Num Eliminations = " << g_el_success << ",   ";
+		Node* p = top;
+		for (int i = 0; i < 20; ++i) {
+			if (p == nullptr) break;
+			std::cout << p->key << " ";
+			p = p->next;
+		}
+		std::cout << std::endl;
+	}
+
+private:
+	bool TryEliminate(ThreadInfo* info, ThreadInfo*& matched_info)
+	{
+		int pos = GetPosition();
+		int expected = EMPTY;	// 처음은 비어 있을 것이라 기대
+
+		int spin_cnt = 0;
+		if (collision[pos].CAS(expected, thread_id)) {				// 기다리기
+			while (spin_cnt < range.current_spin) {
+				_mm_pause();
+				// check elimination (성공이면 return)
+				if (location[thread_id].ptr->id != thread_id) {
+					matched_info = location[thread_id].ptr;			// 스레드 정보 가져오기
+					if (matched_info != nullptr && matched_info->op != info->op) {
+						return true;								// 소거 성공
+					}
+					break;
+				}
+				++spin_cnt;
+			}
+
+			if (true == location[thread_id].CAS(info, nullptr)) {	// 소거 타임아웃 실패 (아무도 찾아오지 않음)
+				collision[pos].CAS(thread_id, EMPTY);				// 충돌 정보 초기화
+				return false;
+			}
+			else {													// 소거 됨 (그새 누군가 가져감)
+				matched_info = location[thread_id].ptr;				// 스레드 정보 가져오기
+				location[thread_id].CAS(matched_info, nullptr);
+				if (matched_info != nullptr && matched_info->op != info->op) {
+					return true;									// 소거 성공
+				}
+				collision[pos].CAS(thread_id, EMPTY);				// 충돌 정보 초기화
+				return false;
+			}
+		}
+		else {
+			int other_id = collision[pos].val;						// 타 스레드 번호
+			if (other_id == EMPTY) return false;					// 다른 스레드가 없으면 실패
+			ThreadInfo* other_info = location[other_id].ptr;		// 스레드 정보 가져오기
+			if (other_info != nullptr && other_info->op != info->op) {	// 다른 스레드가 소거를 시도했음
+				if (true == collision[pos].CAS(other_id, thread_id)) {
+					if (true == location[other_id].CAS(other_info, info)) {	// 소거 성공
+						matched_info = other_info;					// 스레드 정보 가져오기
+						if (matched_info != nullptr && matched_info->op != info->op) {
+							return true;									// 소거 성공
+						}
+						return false;
+					}
+				}
+			}
+
+			if (true == location[thread_id].CAS(info, nullptr)) {	// 소거 타임아웃 실패 (아무도 찾아오지 않음)
+				collision[pos].CAS(thread_id, EMPTY);				// 충돌 정보 초기화
+				return false;
+			}
+			else {													// 소거 됨 (그새 누군가 가져감)
+				matched_info = location[thread_id].ptr;				// 스레드 정보 가져오기
+				location[thread_id].CAS(matched_info, nullptr);
+				if (matched_info != nullptr && matched_info->op != info->op) {
+					return true;									// 소거 성공
+				}
+				collision[pos].CAS(thread_id, EMPTY);				// 충돌 정보 초기화
+				return false;
+			}
+		}
+	}
+
+};
 
 // Improve 소거 기법 (최근 논문 2021)
 class ImprovedLockFreeExchanger
@@ -405,181 +580,6 @@ public:
 };
 
 
-struct LockFreeEliminationStack
-{
-	Node* volatile top;
-	std::vector<ThreadInfoPtr> location;
-	std::vector<Integer> collision;
-
-	LockFreeEliminationStack(int th_num = MAX_THREADS) : top(nullptr), location(th_num), collision(2 * th_num, -1) {}
-
-	bool CAS(Node* old_val, Node* new_val)
-	{
-		return std::atomic_compare_exchange_strong(
-			reinterpret_cast<volatile std::atomic_llong*>(&top), 
-			reinterpret_cast<long long*>(&old_val), 
-			reinterpret_cast<long long>(new_val));
-	}
-
-	int GetPosition()
-	{
-		return (rand() % range.current_range);	// 중앙 집중 방식 (결론적으로)
-		/*	복잡만 하고 위와 비슷한 일을 하는 코드이다
-		int mid = range.max_range / 2;
-		int half_range = range.current_range / 2;
-		if (half_range == 0) return mid;							// 범위가 너무 작음 (zero division 방지)
-		return (rand() % range.current_range) + (mid - half_range);	// 중앙 집중 분포로 서브 논문의 방식
-																	// woudenberg 논문의 11page에서 중앙 집중 분포를 사용한 성능 비교 참고
-																	// 메인 논문에도 있었다 (배열의 중앙 집중 방식이지만 Range를 늘려감)
-		*/
-	}
-
-	void Clear()
-	{
-		while (top != nullptr) {
-			Node* temp = top;
-			top = top->next;
-			delete temp;
-		}
-	}
-
-	void Push(int num)
-	{
-		Node* new_node = new Node(num);
-		ThreadInfo* p = new ThreadInfo{ thread_id, 'P', new_node };	// 스레드 정보 저장
-
-		while (true) {
-			Node* old_top = top;
-			new_node->next = old_top;
-			if (true == CAS(old_top, new_node)) {
-				return;	// 성공적으로 푸시됨 (중앙 스택에 바로,,, 가장 깔끔한 적은 부하의 상황)
-			}
-			// 실패한 경우
-			
-			// 충돌 시도
-			ThreadInfo* matched_info = nullptr;
-			location[thread_id].ptr = p;
-			if (TryEliminate(p, matched_info)) {
-				range.expand();
-				g_el_success++;
-				return;	// 소거 성공
-			}
-			else {
-				range.shrink();	// 범위 축소
-			}
-		}
-	}
-
-	int Pop()
-	{
-		ThreadInfo* p = new ThreadInfo{ thread_id, 'Q', nullptr };	// 스레드 정보 저장
-
-		while (true) {
-			Node* old_top = top;
-			if (old_top == nullptr) {
-				return EMPTY;	// 비어있음
-			}
-			Node* new_top = old_top->next;
-			if (CAS(old_top, new_top)) {
-				int num = old_top->key;
-				//delete old_top;	// 노드 삭제
-				return num;	// 성공적으로 pop 됨
-			}
-
-			ThreadInfo* matched_info = nullptr;
-			location[thread_id].ptr = p;
-			if (TryEliminate(p, matched_info)) {
-				int num = matched_info->node->key;	// p노드가 nullptr임 지역 변수가 사라진 듯 함
-				range.expand();
-				//delete p.node;
-				return num;	
-			}
-			else {
-				range.shrink();	// 범위 축소
-			}
-		}
-	}
-	
-	void Print20()
-	{
-		std::cout << "Num Eliminations = " << g_el_success << ",   ";
-		Node* p = top;
-		for (int i = 0; i < 20; ++i) {
-			if (p == nullptr) break;
-			std::cout << p->key << " ";
-			p = p->next;
-		}
-		std::cout << std::endl;
-	}
-
-private:
-	bool TryEliminate(ThreadInfo* info, ThreadInfo*& matched_info)
-	{
-		int pos = GetPosition();
-		int expected = EMPTY;	// 처음은 비어 있을 것이라 기대
-
-		int spin_cnt = 0;
-		if (collision[pos].CAS(expected, thread_id)) {				// 기다리기
-			while (spin_cnt < range.current_spin) {
-				_mm_pause();
-				// check elimination (성공이면 return)
-				if (location[thread_id].ptr->id != thread_id) {
-					matched_info = location[thread_id].ptr;			// 스레드 정보 가져오기
-					if (matched_info != nullptr && matched_info->op != info->op) {
-						return true;								// 소거 성공
-					}
-					break;
-				}
-				++spin_cnt;
-			}
-
-			if (true == location[thread_id].CAS(info, nullptr)) {	// 소거 타임아웃 실패 (아무도 찾아오지 않음)
-				collision[pos].CAS(thread_id, EMPTY);				// 충돌 정보 초기화
-				return false;
-			}
-			else {													// 소거 됨 (그새 누군가 가져감)
-				matched_info = location[thread_id].ptr;				// 스레드 정보 가져오기
-				location[thread_id].CAS(matched_info, nullptr);
-				if (matched_info != nullptr && matched_info->op != info->op) {
-					return true;									// 소거 성공
-				}
-				collision[pos].CAS(thread_id, EMPTY);				// 충돌 정보 초기화
-				return false;
-			}
-		}
-		else {
-			int other_id = collision[pos].val;						// 타 스레드 번호
-			if (other_id == EMPTY) return false;					// 다른 스레드가 없으면 실패
-			ThreadInfo* other_info = location[other_id].ptr;		// 스레드 정보 가져오기
-			if (other_info != nullptr && other_info->op != info->op) {	// 다른 스레드가 소거를 시도했음
-				if (true == collision[pos].CAS(other_id, thread_id)) {
-					if (true == location[other_id].CAS(other_info, info)) {	// 소거 성공
-						matched_info = other_info;					// 스레드 정보 가져오기
-						if (matched_info != nullptr && matched_info->op != info->op) {
-							return true;									// 소거 성공
-						}
-						return false;
-					}
-				}
-			}
-
-			if (true == location[thread_id].CAS(info, nullptr)) {	// 소거 타임아웃 실패 (아무도 찾아오지 않음)
-				collision[pos].CAS(thread_id, EMPTY);				// 충돌 정보 초기화
-				return false;
-			}
-			else {													// 소거 됨 (그새 누군가 가져감)
-				matched_info = location[thread_id].ptr;				// 스레드 정보 가져오기
-				location[thread_id].CAS(matched_info, nullptr);
-				if (matched_info != nullptr && matched_info->op != info->op) {
-					return true;									// 소거 성공
-				}
-				collision[pos].CAS(thread_id, EMPTY);				// 충돌 정보 초기화
-				return false;
-			}
-		}
-	}
-
-};
 
 LockFreeBackOffStack stack;
 
